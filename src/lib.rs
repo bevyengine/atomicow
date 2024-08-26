c11f8d0aa40059d59e646d3514c5f235104beb26
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-26 19:08:51 -0400

    feat: small tweaks to docs

--- src/lib.rs
+++ src/lib.rs
@@ -9,28 +9,33 @@ use std::{
     sync::Arc,
 };
 
-/// Much like a [`Cow`](std::borrow::Cow), but owned values are Arc-ed to make clones cheap. This should be used for values that
+/// Much like a [`Cow`](std::borrow::Cow), but owned values are [`Arc`]-ed to make clones cheap. This should be used for values that
 /// are cloned for use across threads and change rarely (if ever).
 ///
 /// This also makes an opinionated tradeoff by adding a [`CowArc::Static`] and implementing [`From<&'static T>`] instead of
 /// [`From<'a T>`]. This preserves the static context and prevents conversion to [`CowArc::Owned`] in cases where a reference
 /// is known to be static. This is an optimization that prevents allocations and atomic ref-counting.
 ///
-/// This means that static references should prefer [`From::from`] or [`CowArc::Static`] and non-static references must
+/// This means that static references should prefer [`CowArc::from`] or [`CowArc::Static`] and non-static references must
 /// use [`CowArc::Borrowed`].
 pub enum CowArc<'a, T: ?Sized + 'static> {
-    /// A borrowed value
+    /// A borrowed value.
     Borrowed(&'a T),
-    /// A static value reference. This exists to avoid conversion to [`CowArc::Owned`] in cases where a reference is
+    /// A static value reference.
+    /// 
+    /// This exists to avoid conversion to [`CowArc::Owned`] in cases where a reference is
     /// known to be static. This is an optimization that prevents allocations and atomic ref-counting.
     Static(&'static T),
-    /// An owned [`Arc`]-ed value
+    /// An owned [`Arc`]-ed value.
     Owned(Arc<T>),
 }
 
 impl<T: ?Sized> CowArc<'static, T> {
     /// Indicates this [`CowArc`] should have a static lifetime.
+    /// 
     /// This ensures if this was created with a value `Borrowed(&'static T)`, it is replaced with `Static(&'static T)`.
+    /// It is only possible to call this method if `'a` is `'static`.
+    /// This has no effect if this is `Owned(Arc<T>)`.
     #[inline]
     pub fn as_static(self) -> Self {
         match self {
@@ -70,7 +75,9 @@ impl<'a, T: ?Sized> CowArc<'a, T>
 where
     &'a T: Into<Arc<T>>,
 {
-    /// Converts this into an "owned" value. If internally a value is borrowed, it will be cloned into an "owned [`Arc`]".
+    /// Converts this into an "owned" value.
+    /// 
+    /// If internally a value is borrowed, it will be cloned into an "owned [`Arc`]".
     /// If it is already a [`CowArc::Owned`] or a [`CowArc::Static`], it will remain unchanged.
     #[inline]
     pub fn into_owned(self) -> CowArc<'static, T> {
@@ -81,7 +88,9 @@ where
         }
     }
 
-    /// Clones into an owned [`CowArc<'static>`]. If internally a value is borrowed, it will be cloned into an "owned [`Arc`]".
+    /// Clones into an owned [`CowArc<'static>`].
+    /// 
+    /// If internally a value is borrowed, it will be cloned into an "owned [`Arc`]".
     /// If it is already a [`CowArc::Owned`] or [`CowArc::Static`], the value will be cloned.
     /// This is equivalent to `.clone().into_owned()`.
     #[inline]
@@ -144,7 +153,11 @@ impl Default for CowArc<'static, str> {
     }
 }
 
+// A shortcut, since `Path` does not implement `Default`.
 impl Default for CowArc<'static, Path> {
+    /// Returns an empty [`Path`], wrapped in [`CowArc::Static`].
+    /// 
+    /// This is equivalent to `CowArc::Static(Path::new(""))`.
     fn default() -> Self {
         CowArc::Static(Path::new(""))
     }
